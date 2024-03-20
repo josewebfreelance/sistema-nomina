d31c0b341b5963724fc3464d03f8db3931a6122c
Jose Bobadilla <jbobadilla@is4tech.com>
2024-03-20 09:30:46 -0600

    AGREGAR ADD BUTTON

---
+++ src/layout/FormLayout.tsx
@@ -0,0 +1,62 @@
+import {Box, Button, Grid} from "@mui/material";
+import {Add} from "@mui/icons-material";
+import {TitleComponent} from "../modules/administration/components";
+import {Form, Formik, FormikConfig, FormikValues} from "formik";
+import {ReactElement} from "react";
+
+interface Props {
+    update?: boolean;
+    children: ReactElement | ReactElement[];
+    initialValues: FormikValues;
+    validationSchema: FormikConfig<FormikValues>;
+    onSubmit: () => void;
+    onAdd?: () => void;
+}
+
+
+export const FormLayout = ({update = true, children, initialValues, validationSchema, onSubmit, onAdd}: Props) => {
+    return (
+        <>
+            <Grid
+                container
+                spacing={0}
+                sx={{
+                    backgroundColor: 'white',
+                    borderRadius: 5,
+                    p: 3
+                }}
+                className={'grid-main-container'}
+            >
+                <Box>
+                    <TitleComponent title={update ? 'Editar' : 'Agregar'}/>
+                </Box>
+                <Box sx={{ width: '100%' }}>
+                    <Formik
+                        initialValues={initialValues}
+                        onSubmit={onSubmit}
+                        validationSchema={validationSchema}
+                    >
+                        {
+                            _ => (
+                                <Form>
+                                    <Box className={'form-content'}>
+                                        <Grid container spacing={2}>
+                                            {children}
+                                        </Grid>
+                                    </Box>
+                                    <Button variant="contained" type="submit" > Guardar </Button>
+                                </Form>
+                            )
+                        }
+                    </Formik>
+                </Box>
+                {
+                    update &&
+                    <Button className={'button-add'} onClick={onAdd}>
+                        <Add />
+                    </Button>
+                }
+            </Grid>
+        </>
+    )
+}
